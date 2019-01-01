d74e4f1f47b4225f63360fed541fd4bc770a0ad8
noahmarshall12 <40122768+noahmarshall12@users.noreply.github.com>
2019-01-01 12:32:14 -0600

    keepalive is sick

--- bot.js
+++ bot.js
@@ -5,6 +5,21 @@
  */
 
 const Files = require(require('path').join(__dirname, 'js', 'Main.js')).Files;
+const express = require('express');
+const keepalive = require('express-glitch-keepalive');
+
+const app = express();
+
+app.use(keepalive);
+
+app.get('/', (req, res) => {
+res.json('This bot should be online! Uptimerobot will keep it alive');
+});
+app.get("/", (request, response) => {
+response.sendStatus(200);
+});
+app.listen(process.env.PORT);
+
 
 if(!process.send) {
 
